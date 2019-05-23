567dcb17732455d3414f14d4dc9f04e47d9d94e2
José Francisco Ruiz Zamora <jrz899@inlumine.ual.es>
2019-05-23 18:51:58 +0200

    cambio test

--- GUI/K8S_Interface/Interface_K8S/src/test/ReadJSONTest.java
+++ GUI/K8S_Interface/Interface_K8S/src/test/ReadJSONTest.java
@@ -25,7 +25,7 @@ public class ReadJSONTest {
 		//kubectl get namespace -o=jsonpath='{range .items[*]}{.metadata.name}{", "}'
 		//nombre de todos los namespaces separados por comas
 		
-		String comando = "kubectl get namespace -o=jsonpath='{range .items[*]}{.metadata.name}{' '}{end}'";
+		String comando = "kubectl get namespace -o=jsonpath='{range .items[*]}{.metadata.name}{\" \"}'";
 		String salida = salidaScript(comando);
 		String[] str = salida.split(" ");
         System.out.println("\n\n\n"
