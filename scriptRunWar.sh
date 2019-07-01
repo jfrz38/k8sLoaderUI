54da4a02bb5098f7f114fb77f23ada49ecf73279
jfrz38 <jrz899@inlumine.ual.es>
2019-07-01 16:34:06 +0000

    relative path to script

--- scriptRunWar.sh
+++ scriptRunWar.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-#Ir al directorio donde está el proyecto
-
-#Git pull
-
+#Directorio actual
+CURRENTDIR=$(pwd)
 #Correr con maven para generar el .war
 
-cd /home/ubuntu/aa/TFGDocker/GUI/K8S_Interface/Interface_K8S
+cd $CURRENTDIR/GUI/K8S_Interface/Interface_K8S
 mvn clean install
 
-#Mover el .war a la carpeta de tomcat 
+#Mover el .war a la carpeta de tomcat
 
-mv /home/ubuntu/aa/TFGDocker/GUI/K8S_Interface/Interface_K8S/target/Interface_K8S-0.0.1.war /opt/tomcat/apache-tomcat-9.0.16/webapps
+mv $CURRENTDIR/GUI/K8S_Interface/Interface_K8S/target/Interface_K8S-0.0.1.war /opt/tomcat/apache-tomcat-9.0.16/webapps
