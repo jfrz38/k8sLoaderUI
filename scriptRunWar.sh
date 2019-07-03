82c6162ac3ae6540b3f03788d16f5e95d59a040f
jfrz38 <jrz899@inlumine.ual.es>
2019-07-03 19:36:31 +0000

    Reestructuración archivos

--- scriptRunWar.sh
+++ scriptRunWar.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-#Ir al directorio donde está el proyecto
-
-#Git pull
-
 #Correr con maven para generar el .war
+mydir "$0"
+dirname "$(realpath $0)"
+# echo dirname
 
-cd /home/ubuntu/aa/TFGDocker/GUI/K8S_Interface/Interface_K8S
+cd $(dirname "$(realpath $0)")/GUI/K8S_Interface/Interface_K8S
 mvn clean install
 
 #Mover el .war a la carpeta de tomcat 
 
-mv /home/ubuntu/aa/TFGDocker/GUI/K8S_Interface/Interface_K8S/target/Interface_K8S-0.0.1.war /opt/tomcat/apache-tomcat-9.0.16/webapps
+mv dirname "$(realpath $0)"/GUI/K8S_Interface/Interface_K8S/target/Interface_K8S-0.0.1.war /opt/tomcat/apache-tomcat-9.0.16/webapps
